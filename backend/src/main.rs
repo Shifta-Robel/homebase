90ed99d2f8d89328d9d8506d036024fac4b47696
Shifta-Robel <robelbire16@gmail.com>
2023-08-04 09:26:54 +0300

    used actix error and added logging

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,12 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, ResponseError};
+use actix_web::{
+    get, post, error, App, HttpResponse, HttpServer,
+    http::{header::ContentType, StatusCode},
+    middleware::Logger,
+};
 use actix_cors::Cors;
 use rusqlite::{Connection, Result};
 use serde::{Deserialize, Serialize};
-use std::{fmt, env, fs};
-// use serde_json::json;
+use std::{fmt, fs};
 
 #[derive(Serialize)]
 struct HistoryItem {
@@ -25,41 +28,56 @@ struct Quickmark {
 }
 
 #[derive(Debug)]
-enum ApiError {
+enum ServerError {
     DbError(rusqlite::Error),
     SerializeError(serde_json::Error),
     IOError(std::io::Error),
 }
 
-impl fmt::Display for ApiError {
+impl fmt::Display for ServerError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{:?}", self)
     }
 }
 
-impl ResponseError for ApiError {}
-
-fn error_response(e: ApiError) -> HttpResponse {
-    match e {
-        ApiError::DbError(e) => HttpResponse::InternalServerError().body(format!("{:?}", e)),
-        ApiError::SerializeError(e) => HttpResponse::InternalServerError().body(format!("{:?}", e)),
-        ApiError::IOError(e) => HttpResponse::InternalServerError().body(format!("{:?}", e))
+impl error::ResponseError for ServerError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            ServerError::DbError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            ServerError::SerializeError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            ServerError::IOError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+    fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
+        HttpResponse::build(self.status_code()).insert_header(ContentType::json()).body(self.to_string())
     }
 }
 
-#[get("/get_history")]
-async fn history() -> impl Responder {
-    let conn = Connection::open("/home/robel/.local/share/qutebrowser/history.sqlite").map_err(ApiError::DbError);
-    if let Err(e) = conn {
-        return error_response(e);
-    }
-    let conn = conn.unwrap();
-    let stmt = conn.prepare("SELECT url, title, atime FROM History")
-        .map_err(ApiError::DbError);
-    if let Err(e) = stmt {
-        return error_response(e);
-    }
-    let mut stmt = stmt.unwrap();
+// #[derive(Debug)]
+// enum UserError {
+// }
+//
+// impl fmt::Display for UserError {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         write!(f, "{:?}", self)
+//     }
+// }
+//
+// impl error::ResponseError for UserError {
+//     fn status_code(&self) -> StatusCode {
+//         match self {
+//         }
+//     }
+//     fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
+//         HttpResponse::build(self.status_code()).insert_header(ContentType::json()).body(self.to_string())
+//     }
+// }
+
+#[get("/history")]
+async fn history() -> Result<HttpResponse, ServerError>{
+    let conn = Connection::open("/home/robel/.local/share/qutebrowser/history.sqlite").map_err(ServerError::DbError)?;
+    let mut stmt = conn.prepare("SELECT url, title, atime FROM History")
+        .map_err(ServerError::DbError)?;
     let history = stmt.query_map([], |row|
         Ok(
             HistoryItem {
@@ -68,27 +86,19 @@ async fn history() -> impl Responder {
                 time: row.get(2)?
             }
         )
-    ).map_err(ApiError::DbError);
-    if let Err(e) = history {
-        return error_response(e);
-    }
-    let history: Vec<_> = history.unwrap().map(|item| item.unwrap()).collect();
-    let response = serde_json::to_string(&history).map_err(ApiError::SerializeError);
-    if let Err(e) = response {
-        return error_response(e);
-    }
-    HttpResponse::Ok()
-      .content_type("application/json")
-      .body(response.unwrap())
+    ).map_err(ServerError::DbError)?;
+    let history: Vec<HistoryItem> = history.map(|item| item.unwrap()).collect();
+    let response = serde_json::to_string(&history).map_err(ServerError::SerializeError)?;
+    Ok(
+        HttpResponse::Ok()
+          .content_type("application/json")
+          .body(response)
+    )
 }
 
 #[get("/bookmarks")]
-async fn bookmarks() -> impl Responder {
-    let contents = fs::read_to_string("/home/robel/.config/qutebrowser/bookmarks/urls").map_err(ApiError::IOError);
-    if let Err(e) = contents {
-        return error_response(e);
-    }
-    let contents = contents.unwrap();
+async fn bookmarks() -> Result<HttpResponse, ServerError> {
+    let contents = fs::read_to_string("/home/robel/.config/qutebrowser/bookmarks/urls").map_err(ServerError::IOError)?;
     let lines = contents.lines();
     let bookmark_vector: Vec<_> = lines.map(|line| {
         let idx = line.find(' ').unwrap();
@@ -97,22 +107,17 @@ async fn bookmarks() -> impl Responder {
             title: line[idx+1..].to_string(),
         }
     }).collect();
-    let response = serde_json::to_string(&bookmark_vector).map_err(ApiError::SerializeError);
-    if let Err(e) = response {
-        return error_response(e);
-    }
-    HttpResponse::Ok()
-      .content_type("application/json")
-      .body(response.unwrap())
+    let response = serde_json::to_string(&bookmark_vector).map_err(ServerError::SerializeError)?;
+    Ok(
+        HttpResponse::Ok()
+          .content_type("application/json")
+          .body(response)
+    )
 }
 
 #[get("/quickmarks")]
-async fn quickmarks() -> impl Responder {
-    let contents = fs::read_to_string("/home/robel/.config/qutebrowser/quickmarks").map_err(ApiError::IOError);
-    if let Err(e) = contents {
-        return error_response(e);
-    }
-    let contents = contents.unwrap();
+async fn quickmarks() -> Result<HttpResponse, ServerError> {
+    let contents = fs::read_to_string("/ome/robel/.config/qutebrowser/quickmarks").map_err(ServerError::IOError)?;
     let lines = contents.lines();
     let quickmark_vector: Vec<_> = lines.map(|line| {
         let idx = line.find(' ').unwrap();
@@ -121,19 +126,21 @@ async fn quickmarks() -> impl Responder {
             url: line[idx+1..].to_string(),
         }
     }).collect();
-    let response = serde_json::to_string(&quickmark_vector).map_err(ApiError::SerializeError);
-    if let Err(e) = response {
-        return error_response(e);
-    }
-    HttpResponse::Ok()
-      .content_type("application/json")
-      .body(response.unwrap())
+    let response = serde_json::to_string(&quickmark_vector).map_err(ServerError::SerializeError)?;
+    Ok(
+        HttpResponse::Ok()
+          .content_type("application/json")
+          .body(response)
+    )
 }
-
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // std::env::set_var("RUST_LOG", "info");
+    // std::env::set_var("RUST_BACKTRACE", "1");
+    // env_logger::init();
     HttpServer::new(|| {
+        // let logger = Logger::default();
         App::new()
             .wrap(
                 Cors::default()
@@ -141,6 +148,7 @@ async fn main() -> std::io::Result<()> {
                     .allow_any_header()
                     .allow_any_method()
             )
+            // .wrap(logger)
             .service(history)
             .service(bookmarks)
             .service(quickmarks)
