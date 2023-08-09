ac1d8e2e7b94060bdfd8bae8170fa81aa7c9c6fa
Shifta-Robel <robelbire16@gmail.com>
2023-08-09 10:50:40 +0300

    moved quickmark path loading up to api/quickmark

--- backend/src/api/quickmarks.rs
+++ backend/src/api/quickmarks.rs
@@ -2,11 +2,12 @@ use actix_web::{web, HttpResponse, HttpRequest};
 use rusqlite::Result;
 use crate::config::AppConfig;
 use crate::errors::ServerError;
-// use crate::models::Quickmark;
+use crate::models::quickmark::Quickmark;
 use crate::services::quickmarks::get_quickmarks;
 
 pub async fn quickmarks(_req: HttpRequest, config: web::Data<AppConfig>) -> Result<HttpResponse, ServerError> {
-    let quickmark_vector = get_quickmarks(&config)?;
+    let path = config.quickmark_file_path.clone().unwrap_or(Quickmark::get_default_path());
+    let quickmark_vector = get_quickmarks(&path)?;
     let response = serde_json::to_string(&quickmark_vector).map_err(ServerError::SerializeError)?;
     Ok(
         HttpResponse::Ok()
--- backend/src/services/quickmarks.rs
+++ backend/src/services/quickmarks.rs
@@ -1,11 +1,10 @@
 use rusqlite::Result;
 use std::fs;
-use crate::config::AppConfig;
 use crate::errors::ServerError;
 use crate::models::quickmark::Quickmark;
 
-pub fn get_quickmarks(config: &AppConfig) -> Result<Vec<Quickmark>, ServerError> {
-    let path = config.quickmark_file_path.clone().unwrap_or(Quickmark::get_default_path());
+pub fn get_quickmarks(path: &str) -> Result<Vec<Quickmark>, ServerError> {
+    // let path = config.quickmark_file_path.clone().unwrap_or(Quickmark::get_default_path());
     let contents = fs::read_to_string(path)
         .map_err(ServerError::IOError)?;
     let lines = contents.lines();
